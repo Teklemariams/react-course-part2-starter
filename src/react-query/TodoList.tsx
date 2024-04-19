ba7bebe4683a9a781eacb33a92661728b0e8e599
teklemariam <teklemarry@gmail.com>
2024-04-19 13:16:04 +0300

    6-Showing a loading indicator

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -14,11 +14,16 @@ const TodoList = () => {
       .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.data);
   //specify the type of data and error
-  const { data: todos, error } = useQuery<Todo[], Error>({
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
   });
 
+  if (isLoading) return <p>Loading...</p>; //spinner is also possible
   if (error) return <p>{error.message}</p>;
 
   return (
