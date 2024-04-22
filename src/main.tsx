b572e618f09bbb644b6629fc4e375c9f23101290
teklemariam <teklemarry@gmail.com>
2024-04-22 13:38:53 +0300

    8-Usin ReactQueryDevTools

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient(); //forgot the ()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      {<App /> /*  App shall be inside */}
+      <App />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
